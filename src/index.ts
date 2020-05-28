3d7c6a88776fb2603ba6c307d43878ba12589f11
511V41 <yukkuri.sinai@gmail.com>
2020-05-28 22:50:45 +0900

    オーナー名の取得

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,21 @@ import { promises as fs } from "fs";
 import { program } from "commander";
 
 import { version } from "../package.json";
+import map from "./map";
 
 program
   .version(version)
   .option("-f, --file <path>", "file path")
   .parse(process.argv);
+
+// なぜかゴミが入ってることがあるので消す
+const deleteCD = (buf: Buffer): Buffer => {
+  const index = buf.indexOf(0xcd);
+  if (index !== -1) {
+    return buf.slice(0, index);
+  }
+  return buf;
+};
 
 const main = async () => {
   const path = program.file;
@@ -15,8 +25,16 @@ const main = async () => {
     console.error("ファイルを読み込めませんでした。", err);
     process.exit(1);
   });
-  const teamName = iconv.decode(data.slice(640, 664), "Shift_JIS");
+  const teamName = iconv.decode(
+    deleteCD(data.slice(...map.teamName)),
+    "Shift_JIS"
+  );
   console.info(teamName);
+  const ownerName = iconv.decode(
+    deleteCD(data.slice(...map.ownerName)),
+    "Shift_JIS"
+  );
+  console.info(ownerName);
 };
 
 main();
---
+++ src/map.ts
@@ -0,0 +1,6 @@
+const map = {
+  teamName: [640, 664],
+  ownerName: [665, 689]
+};
+
+export default map;
