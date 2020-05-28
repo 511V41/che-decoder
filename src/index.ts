ae8712b978a62f05d230278f7697fd948b025820
511V41 <yukkuri.sinai@gmail.com>
2020-05-28 23:23:43 +0900

    識別名の取得

--- src/index.ts
+++ src/index.ts
@@ -19,22 +19,60 @@ const deleteCD = (buf: Buffer): Buffer => {
   return buf;
 };
 
+// 識別名が初期値か？
+const isMachineNameInitialValue = (buf: Buffer): boolean => {
+  const index = buf.indexOf(0xff);
+  if (index !== -1) {
+    return true;
+  }
+  return false;
+};
+
 const main = async () => {
   const path = program.file;
   const data = await fs.readFile(path).catch(err => {
     console.error("ファイルを読み込めませんでした。", err);
     process.exit(1);
   });
-  const teamName = iconv.decode(
-    deleteCD(data.slice(...map.teamName)),
-    "Shift_JIS"
+  console.info(
+    iconv.decode(deleteCD(data.slice(...map.teamName)), "Shift_JIS")
   );
-  console.info(teamName);
-  const ownerName = iconv.decode(
-    deleteCD(data.slice(...map.ownerName)),
-    "Shift_JIS"
+  console.info(
+    iconv.decode(deleteCD(data.slice(...map.ownerName)), "Shift_JIS")
   );
-  console.info(ownerName);
+
+  const machineName1 = deleteCD(data.slice(...map.machineName1));
+  if (isMachineNameInitialValue(machineName1)) {
+    console.info("初期値");
+  } else {
+    console.info(iconv.decode(machineName1, "Shift_JIS"));
+  }
+  const machineName2 = deleteCD(data.slice(...map.machineName2));
+  if (isMachineNameInitialValue(machineName2)) {
+    console.info("初期値");
+  } else {
+    const name = iconv
+      .decode(machineName2, "Shift_JIS")
+      .replace(/\0[\s\S]*$/g, "");
+    if (name === "") {
+      console.info("初期値");
+    } else {
+      console.info(name);
+    }
+  }
+  const machineName3 = deleteCD(data.slice(...map.machineName3));
+  if (isMachineNameInitialValue(machineName3)) {
+    console.info("初期値");
+  } else {
+    const name = iconv
+      .decode(machineName3, "Shift_JIS")
+      .replace(/\0[\s\S]*$/g, "");
+    if (name === "") {
+      console.info("初期値");
+    } else {
+      console.info(name);
+    }
+  }
 };
 
 main();
--- src/map.ts
+++ src/map.ts
@@ -1,6 +1,12 @@
 const map = {
+  // チーム名
   teamName: [640, 664],
-  ownerName: [665, 689]
+  // オーナー名
+  ownerName: [665, 689],
+  // 各機体1の識別名
+  machineName1: [884, 908],
+  machineName2: [8756, 8780],
+  machineName3: [16628, 16652]
 };
 
 export default map;
